cceb9b3cb7f0d50b2119951ac28a51bd9925e493
chenershuai <chenershuaizg@163.com>
2018-01-30 13:59:10 +0800

    first commit

---
+++ app/src/main/java/com/jackchen/baidudemo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.jackchen.baidudemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.map.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mapView = (MapView) findViewById(R.id.mapView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // activity 暂停时同时暂停地图控件
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // activity 恢复时同时恢复地图控件
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // activity 销毁时同时销毁地图控件
+        MapView.setMapCustomEnable(false);
+        mapView.onDestroy();
+    }
+}
